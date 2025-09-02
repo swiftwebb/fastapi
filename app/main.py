bf348cbb261d76806e332e0a2abaf5a526dcb4b3
swiftwebb <swiftwebbb@gmail.com>
2025-09-02 01:51:03 +0100

    yoo2

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"message": "Hello World nice"}
+    return {"message": "Hello World "}
 
 
 # @app.get("/posts", response_model= List[schemas.Post])
